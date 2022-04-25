1aae8e2bd708ad468058dcd8bf6fb8e24d112579
Ilya Panteleychuk <panteleychukilia@gmail.com>
2022-04-25 19:26:13 +0300

    Changed logic of calculateUniqueChars method with adding ComputeIfAbsent method

--- src/main/java/ua/foxminded/ilyapanteleychuk/charcounter/charcounter/CharCountImplementation.java
+++ src/main/java/ua/foxminded/ilyapanteleychuk/charcounter/charcounter/CharCountImplementation.java
@@ -11,9 +11,9 @@ public class CharCountImplementation implements CharCount {
     private static final Map<String, Map<Character, Integer>> cache = new LinkedHashMap<>();
 
     @Override
-    public Map<Character, Integer> charCount(String input){
+    public Map<Character, Integer> charCount(String input) {
         Map<Character, Integer> cached = cache.get(input);
-        if(cached != null){
+        if(cached != null) {
             return cached;
         }
         Map<Character, Integer> calculated = calculateUniqueChars(input);
@@ -21,21 +21,20 @@ public class CharCountImplementation implements CharCount {
         return calculated;
     }
 
-    private Map<Character, Integer> calculateUniqueChars(String input){
+    private Map<Character, Integer> calculateUniqueChars(String input) {
         Map<Character, Integer> uniqueChars = new LinkedHashMap<>();
         List<Character> inputCharList = input.toLowerCase().chars().
-                    mapToObj(c -> (char) c).collect(Collectors.toList());
-        for(int i = 0; i != inputCharList.size();){
-            int value = 1;
-            uniqueChars.put(inputCharList.get(i), value);
-            for(int j = i + 1; j != inputCharList.size(); j++){
-                if(inputCharList.get(j).equals(inputCharList.get(i))){
-                    value++;
-                    uniqueChars.replace(inputCharList.get(i), value);
-                    inputCharList.remove(j);
-                    j--;
+            mapToObj(c -> (char) c).collect(Collectors.toList());
+        for(int i = 0; i < inputCharList.size();) {
+            uniqueChars.computeIfAbsent(inputCharList.get(i), character -> {
+                int result = 1;
+                for(int j = i + 1; j < inputCharList.size(); j++) {
+                    if(inputCharList.get(i).equals(inputCharList.get(j))) {
+                        result++;
+                    }
                 }
-            }
+                return result;
+            });
             inputCharList.remove(i);
         }
         return uniqueChars;
